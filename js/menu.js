44a60d9f67334ddc6a6829beff0c82b286c9d0bd
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2016-02-14 13:32:29 +0100

    Menu tiles actions

--- js/menu.js
+++ js/menu.js
@@ -3,17 +3,10 @@ var itemArray = [
     { title: "Paradux", text: "Puzzle", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#530" },
     { title: "Another game", text: "?", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#a00" },
     { title: "JSonic the Edgehog", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#3ac" },
-    
-    { title: "Not a Cat", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#999" },
-    { title: "Paradux", text: "Puzzle", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#530" },
-    { title: "Another game", text: "?", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#a00" },
-    { title: "JSonic the Edgehog", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#3ac" },
-    
-    { title: "Not a Cat", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#999" },
-    { title: "Paradux", text: "Puzzle", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#530" },
-    { title: "Another game", text: "?", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#a00" },
-    { title: "JSonic the Edgehog", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#3ac" }
+    { title: "Reboot", text: "System", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#555",action:function(){location="splash.html"} },
 ];
+
+itemArray.forEach(function(x,id){x.tileId=id});
 
 
 var tiles=[];
@@ -23,11 +16,24 @@ WinJS.Namespace.define("Sample.ListView", {
 WinJS.UI.processAll();
 
 
+var itemArrayCopy=itemArray.map(function(x){return x});
+
 function addTile(){
-    if(itemArray.length>0){
-        Sample.ListView.data.push(itemArray.pop());
+    if(itemArrayCopy.length>0){
+        Sample.ListView.data.push(itemArrayCopy.shift());
         setTimeout(addTile,200);
     }
 }
 
 addTile();
+
+function keydownTile(e){
+    alert(e);
+}
+
+function clickTile(e){
+    var l=itemArray.filter(function(x){return x.tileId==e});
+    if(l.length>0 && l[0].action){
+        l[0].action()
+    }
+}
