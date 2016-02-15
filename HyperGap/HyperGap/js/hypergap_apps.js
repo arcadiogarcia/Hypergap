633bfeb1422041374a3083cc929be2ce71b3982a
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2016-02-15 23:55:43 +0100

    .hgp decompressing and manifest loading

---
+++ HyperGap/HyperGap/js/hypergap_apps.js
@@ -0,0 +1,51 @@
+﻿zip.workerScriptsPath = "/js/";
+
+function installGameFromLocaFile() {
+    // Create the picker object and set options
+    var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+    openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.list;
+    openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+    // Users expect to have a filtered view of their folders depending on the scenario.
+    // For example, when choosing a documents folder, restrict the filetypes to documents for your application.
+    openPicker.fileTypeFilter.replaceAll([".hgp"]);
+
+
+    openPicker.pickSingleFileAsync().then(function (file) {
+        return file.openAsync(Windows.Storage.FileAccessMode.read);
+    }).done(function (fileBuf) {
+        var blob = MSApp.createBlobFromRandomAccessStream('application/zip', fileBuf);
+        // use a BlobReader to read the zip from a Blob object
+        zip.createReader(new zip.BlobReader(blob), function (reader) {
+
+            // get all entries from the zip
+            reader.getEntries(function (entries) {
+                if (entries.length) {
+
+                    console.log(entries);
+                    var manifest = entries.filter(function (x) { return x.filename == "manifest.json" });
+                    if (manifest.length == 0) {
+                        //TODO: ERROR, NO MANIFEST
+                    }
+                    manifest = manifest[0];
+                    manifest.getData(new zip.TextWriter(), function (text) {
+                        // text contains the entry data as a String
+                        var manifestData = JSON.parse(text);
+                        console.log(manifestData);
+
+                        // close the zip reader
+                        reader.close(function () {
+                            // onclose callback
+                        });
+
+                    }, function (current, total) {
+                        // onprogress callback
+                    });
+
+                }
+            });
+        }, function (error) {
+            // onerror callback
+        });
+    });
+
+}
--- HyperGap/HyperGap/js/menu.js
+++ HyperGap/HyperGap/js/menu.js
@@ -3,13 +3,14 @@ var itemArray = [
     { title: "Paradux", text: "Puzzle", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#530" },
     { title: "Another game", text: "?", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#a00" },
     { title: "JSonic the Edgehog", text: "Platformer", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#3ac" },
-    { title: "Reboot", text: "System", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#555",action:function(){location="splash.html"} },
+{ title: "Install game", text: "System", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#555", action: function () { installGameFromLocaFile(); } },
+{ title: "Reboot", text: "System", picture: "http://try.buildwinjs.com/images/fruits/60Mint.png", color: "#555", action: function () { location = "splash.html" } },
 ];
 
-itemArray.forEach(function(x,id){x.tileId=id});
+itemArray.forEach(function (x, id) { x.tileId = id });
 
 
-var tiles=[];
+var tiles = [];
 WinJS.Namespace.define("Sample.ListView", {
     data: new WinJS.Binding.List(tiles)
 });
@@ -18,20 +19,20 @@ WinJS.UI.processAll().then(function () {
 });
 
 
-var itemArrayCopy=itemArray.map(function(x){return x});
+var itemArrayCopy = itemArray.map(function (x) { return x });
 
-function addTile(){
-    if(itemArrayCopy.length>0){
+function addTile() {
+    if (itemArrayCopy.length > 0) {
         Sample.ListView.data.push(itemArrayCopy.shift());
-        setTimeout(addTile,200);
+        setTimeout(addTile, 200);
     }
 }
 
 addTile();
 
 function clickTile(e) {
-    var l=itemArray.filter(function(x){return x.tileId==e.detail.itemIndex});
-    if(l.length>0 && l[0].action){
+    var l = itemArray.filter(function (x) { return x.tileId == e.detail.itemIndex });
+    if (l.length > 0 && l[0].action) {
         l[0].action()
     }
 }
