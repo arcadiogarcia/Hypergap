a9f95c21e501c615386d43f18d5e3d910e6babe3
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2016-03-24 00:39:39 +0100

    Flappy ninja, udp broadcast

---
+++ HyperGap/HyperGap/js/networkDiscovery.js
@@ -0,0 +1,22 @@
+﻿var datagramSocket = new Windows.Networking.Sockets.DatagramSocket();
+datagramSocket.onmessagereceived = function (e, data) {
+    var rawString = e.getDataReader().readString(15);
+    while (rawString.indexOf("$")!=-1) {
+        rawString=rawString.replace("$", "");
+    }
+    console.log("Just received ",rawString);
+};
+datagramSocket.bindEndpointAsync(null, "8778").done(function () {
+    datagramSocket.joinMulticastGroup(new Windows.Networking.HostName("224.0.0.1"));
+    datagramSocket.getOutputStreamAsync(new Windows.Networking.HostName("224.0.0.1"), "8778").done(function (stream) {
+        var myIP = Windows.Networking.Connectivity.NetworkInformation.getHostNames().filter(x=>x.type == 1).map(x=>x.rawName)[0];
+        while (myIP.length < 15) {
+            myIP += "$";
+        }
+        stream.writeAsync(Windows.Security.Cryptography.CryptographicBuffer.convertStringToBinary(myIP, Windows.Security.Cryptography.BinaryStringEncoding.utf8));
+    });
+}, function (e) {
+    console.log(e);
+}, function (e) {
+    console.log(e);
+});
