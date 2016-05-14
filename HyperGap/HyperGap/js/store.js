e7ecd9c4652290eaf7fd9d4ae494c77df28b45e4
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2016-05-14 18:59:08 +0200

    Store navigation and search

--- HyperGap/HyperGap/js/store.js
+++ HyperGap/HyperGap/js/store.js
@@ -8,26 +8,15 @@ WinJS.Namespace.define("Sample.ListView", {
     data: new WinJS.Binding.List(tiles)
 });
 WinJS.UI.processAll().then(function () {
-    document.getElementById("listView").winControl.addEventListener("iteminvoked", clickTile);
+    //document.getElementById("listView").winControl.addEventListener("iteminvoked", clickTile
+
+    navigationActions.topGames();
 });
 
 
 var itemArrayCopy;
 
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            var apps=JSON.parse(httpRequest.responseText).entries;
-            itemArray = itemArray.concat(apps.map(function (x) { return { title: x.title._, text: x.description._, picture: x.icon._, color: x.color._, size: x.size._, publisher: x.publisher._, action: function () { HYPERGAP.apps.installGameFromURL("http://hypergap.azurewebsites.net/API/games/" + x.RowKey._+"/download", function () { HYPERGAP.CONTROLLER.close(); location = "menu.html"; }); } } }));
-            showMenu();
-        } else {
-            alert('There was a problem with the request.');
-        }
-    }
-};
-httpRequest.open('GET', "http://hypergap.azurewebsites.net/API/games");
-httpRequest.send();
+
 
 
 
@@ -39,6 +28,9 @@ function addTile() {
 }
 
 function showMenu() {
+    while (Sample.ListView.data.length > 0) {
+        Sample.ListView.data.pop();
+    }
     itemArrayCopy = itemArray.map(function (x) { return x });
     itemArray.forEach(function (x, id) { x.tileId = id });
     addTile();
@@ -49,6 +41,10 @@ function clickTile(e) {
     if (l.length > 0 && l[0].action) {
         l[0].action()
     }
+}
+
+function clickPublisher(e) {
+    navigationActions.searchPublisher(this.innerText);
 }
 
 function showLoader(msg, progress) {
@@ -69,4 +65,93 @@ HYPERGAP.MENU.invoke = function (e) {
     if (l.length > 0 && l[0].action) {
         l[0].action()
     }
+}
+
+
+
+//Navigation stack
+var navigationStack = [];
+var navigationActions = {};
+var currentState = null;
+
+function navigateState(newstate) {
+    if (currentState) {
+        navigationStack.push(currentState);
+    }
+    currentState = newstate;
+}
+
+navigationActions.goBack = function () {
+    if (navigationStack.length == 0) {
+        window.location = "menu.html";
+    } else {
+        var previous = navigationStack.pop();
+        navigationActions[previous.action](previous.args, true);
+        currentState = previous;
+    }
+}
+
+navigationActions.topGames = function (args, back) {
+    if (!back) {
+        navigateState({ action: "topGames" });
+    }
+    document.getElementById("textHeader").innerHTML = "Top Games";
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var apps = JSON.parse(httpRequest.responseText).entries;
+                itemArray = apps.map(function (x) { return { title: x.title._, text: x.description._, picture: x.icon._, color: x.color._, size: x.size._, publisher: x.publisher._, action: function () { HYPERGAP.apps.installGameFromURL("http://hypergap.azurewebsites.net/API/games/" + x.RowKey._ + "/download", function () { HYPERGAP.CONTROLLER.close(); location = "menu.html"; }); } } });
+                showMenu();
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    };
+    httpRequest.open('GET', "http://hypergap.azurewebsites.net/API/games");
+    httpRequest.send();
+}
+
+navigationActions.search = function (query, back) {
+    if (!back) {
+        navigateState({ action: "search", args: query });
+    }
+    document.getElementById("textHeader").innerHTML = "Results for '" + query + "'";
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var apps = JSON.parse(httpRequest.responseText).entries;
+                itemArray = apps.map(function (x) { return { title: x.title._, text: x.description._, picture: x.icon._, color: x.color._, size: x.size._, publisher: x.publisher._, action: function () { HYPERGAP.apps.installGameFromURL("http://hypergap.azurewebsites.net/API/games/" + x.RowKey._ + "/download", function () { HYPERGAP.CONTROLLER.close(); location = "menu.html"; }); } } });
+                showMenu();
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    };
+    httpRequest.open('GET', "http://hypergap.azurewebsites.net/API/games/search/" + query);
+    httpRequest.send();
+
+
+}
+
+navigationActions.searchPublisher = function (p, back) {
+    if (!back) {
+        navigateState({ action: "searchPublisher", args: p });
+    }
+    document.getElementById("textHeader").innerHTML = "Games from " + p;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                var apps = JSON.parse(httpRequest.responseText).entries;
+                itemArray = apps.map(function (x) { return { title: x.title._, text: x.description._, picture: x.icon._, color: x.color._, size: x.size._, publisher: x.publisher._, action: function () { HYPERGAP.apps.installGameFromURL("http://hypergap.azurewebsites.net/API/games/" + x.RowKey._ + "/download", function () { HYPERGAP.CONTROLLER.close(); location = "menu.html"; }); } } });
+                showMenu();
+            } else {
+                alert('There was a problem with the request.');
+            }
+        }
+    };
+    httpRequest.open('GET', "http://hypergap.azurewebsites.net/API/games/publisher/" + p);
+    httpRequest.send();
 }
